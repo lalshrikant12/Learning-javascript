2259c58893389239f35e144f12e115fe88c1cbde
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-06-13 23:41:47 +0530

    Create stalker.js

---
+++ Stalker Exercise/stalker.js
@@ -0,0 +1,6 @@
+var firstName= prompt("What is your firstName");
+var lastName=prompt("What is your lastName");
+var age= prompt("What is your age?");
+
+console.log("Your full name is "  + firstName + " " + lastName);
+console.log("You are " +age + " years old");
